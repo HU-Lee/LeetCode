10f0c37ee503a9d2a45710ea9bfd18f8d283efd4
LHU <haulrest@gmail.com>
2024-02-10 15:20:51 +0900

    Time: 142 ms (23.5%), Space: 12.2 MB (61.24%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -1,13 +1,12 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-	for i := 0; i < len(nums); i++ {
-		for j := i+1; j < len(nums); j++ {
-            if j - i > k {
-                break;
-            }
-            if nums[i] == nums[j] {
+	numMap := make(map[int]int)
+    for i:=0; i<len(nums); i++ {
+        idx, exists := numMap[nums[i]]; if exists {
+            if i - idx <= k {
                 return true;
             }
         }
+        numMap[nums[i]] = i;
     }
 	return false;
 }
