8d8217bce8f88f0d7d6c6b3dcae3eef60cc676ef
LHU <haulrest@gmail.com>
2024-02-12 01:22:09 +0900

    Time: 64 ms (17.43%), Space: 51.3 MB (22.78%) - LeetHub

---
+++ 2620-counter/2620-counter.ts
@@ -0,0 +1,16 @@
+function createCounter(n: number): () => number {
+    
+    let i = 0;
+
+    return function() {
+        return n + i++;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
