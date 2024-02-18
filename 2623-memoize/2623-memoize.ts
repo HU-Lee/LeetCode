f9954b358a7e74848b220484a4604f2663fbb780
LHU <haulrest@gmail.com>
2024-02-18 17:17:17 +0900

    Time: 344 ms (16.49%), Space: 84.7 MB (98.02%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,28 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+
+    const dic = new Map()
+    
+    return function(...args: number[]) {
+        const key = args.join("_")
+        if (dic.get(key) !== null && dic.get(key) !== undefined) {
+            return dic.get(key)
+        } else {
+            dic.set(key, fn(...args))
+            return dic.get(key)
+        }   
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
