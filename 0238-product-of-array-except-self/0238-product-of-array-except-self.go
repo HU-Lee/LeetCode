0bb051db3f90c0162d21d7d2e05da2c507505b53
LHU <haulrest@gmail.com>
2024-03-15 11:21:10 +0900

    Time: 28 ms (25.67%), Space: 7.4 MB (60.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,18 +1,16 @@
 func productExceptSelf(nums []int) []int {
     l := len(nums)
     res := make([]int, l)
-    after := make([]int, l)
     res[0] = 1
+    val := 1
     for i := 1; i < l; i++ {
-        res[i] = res[i-1]*nums[i-1]
+        val *= nums[i-1]
+        res[i] = val
     }
-    after[l-1] = 1
+    val = 1
     for i := l-2; i>=0; i-- {
-        after[i] = after[i+1]*nums[i+1]
+        val *= nums[i+1]
+        res[i] *= val
     } 
-    
-    for i := 0; i < l; i++ {
-        res[i] *= after[i]
-    }
     return res
 }
