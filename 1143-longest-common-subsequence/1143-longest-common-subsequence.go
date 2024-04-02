e753ee4dafcacb6b234736316a35991baad81a23
LHU <haulrest@gmail.com>
2024-04-02 19:13:06 +0900

    Time: 44 ms (18.82%), Space: 19.7 MB (21.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.go
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.go
@@ -1,23 +1,19 @@
-var dic map[[2]int]int
-
-func dp(text1 string, text2 string, x int, y int) int {
-    key := [2]int{x,y}
-    if _, ok := dic[key]; !ok {
-        if x == 0 || y == 0 {
-            dic[key] = 0
-        } else if text1[x-1] == text2[y-1] {
-            dic[key] = dp(text1, text2, x-1, y-1) + 1
-        } else {
-            dic[key] = max(
-                dp(text1, text2, x, y-1),
-                dp(text1, text2, x-1, y),
-            )
-        }
-    }
-    return dic[key]
-}
 
 func longestCommonSubsequence(text1 string, text2 string) int {
-    dic = make(map[[2]int]int)
-    return dp(text1, text2, len(text1), len(text2))
+    arr := make([][]int, len(text1)+1)
+    for i := range arr {
+        arr[i] = make([]int, len(text2)+1)
+    }
+    for y := 0; y <= len(text2); y++ {
+        for x := 0; x <= len(text1); x++ {
+            if x == 0 || y == 0 {
+                arr[x][y] = 0
+            } else if text1[x-1] == text2[y-1] {
+                arr[x][y] = arr[x-1][y-1] + 1
+            } else {
+                arr[x][y] = max(arr[x-1][y], arr[x][y-1])
+            }
+        }
+    }
+    return arr[len(text1)][len(text2)]
 }
