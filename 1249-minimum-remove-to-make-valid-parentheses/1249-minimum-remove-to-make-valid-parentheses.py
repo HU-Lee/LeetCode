ccc246c8ec2e90eda8bcf9e782242f04cb712775
LHU <haulrest@gmail.com>
2024-04-06 15:17:31 +0900

    Time: 64 ms (93.09%), Space: 13.6 MB (98.31%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -4,28 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        tmp = ""
-        cnt = 0
-        for x in s:
+        l = list(s)
+        arr = []
+        for idx, x in enumerate(l):
             if x == "(":
-                cnt += 1
+                arr.append(idx)
             elif x == ")":
-                cnt -= 1
-            if cnt < 0:
-                cnt = 0
-                continue
-            tmp += x
+                if arr:
+                    arr.pop()
+                else:
+                    l[idx] = ''
         
-        ans = ""
-        cnt = 0
-        for x in reversed(tmp):
-            if x == ")":
-                cnt += 1
-            elif x == "(":
-                cnt -= 1
-            if cnt < 0:
-                cnt = 0
-                continue
-            ans = x + ans
+        for i in arr:
+            l[i] = ''
 
-        return ans
+        return ''.join(l)
