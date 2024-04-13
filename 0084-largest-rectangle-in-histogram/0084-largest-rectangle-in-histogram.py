7a1f7e0eb28af8f5ebff0d0605a88e166a19d07f
LHU <haulrest@gmail.com>
2024-04-13 15:13:04 +0900

    Time: 652 ms (82.91%), Space: 33.7 MB (40.27%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,28 +1,15 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         l = len(heights)
-        st = []
-        bd_right = [i for i in range(l)]
-        for idx, h in enumerate(heights):
-            while st and st[-1][0] > h:
-                x,i = st.pop()
-                bd_right[i] = idx-1
-            st.append((h, idx))
-        while st:
-            x,i = st.pop()
-            bd_right[i] = l-1
-        bd_left = [i for i in range(l)]
-        for idx, h in enumerate(heights[::-1]):
-            while st and st[-1][0] > h:
-                x,i = st.pop()
-                bd_left[i] = l-idx
-            st.append((h, l-1-idx))
-        while st:
-            x,i = st.pop()
-            bd_left[i] = 0
-
         ans = 0
-        for i in range(l):
-            tmp = heights[i]*(bd_right[i] - bd_left[i] + 1)
-            ans = max(ans, tmp)
+        st = []
+        for idx, h in enumerate(heights):
+            start = idx
+            while st and st[-1][0] > h:
+                x,i = st.pop()
+                ans = max(ans, x*(idx - i))
+                start = i
+            st.append((h, start))
+        for x,i in st:
+            ans = max(ans, x*(l-i))
         return ans
