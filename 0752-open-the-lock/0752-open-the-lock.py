9d410f85b8f35ff3e2897ca39cdf7a675870830b
Austin Lee <haulrest@gmail.com>
2024-04-22 10:30:29 +0900

    Time: 1550 ms (8.2%), Space: 17.6 MB (94.44%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,34 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+
+        def changeLock(lock: str, idx: int, reverse: bool) -> str:
+            numbers = "0123456789"    
+            v = int(lock[i])    
+            lock_arr = list(lock)
+            if reverse:
+                lock_arr[i] = numbers[v-1]
+            else:
+                lock_arr[i] = numbers[(v+1)%10]
+            return "".join(lock_arr)
+
+        q = [("0000",0)]
+        visited = {
+            "0000": True
+        }
+
+        while q:
+            lock, x = q.pop(0)
+            if lock == target:
+                return x
+            elif lock in deadends:
+                continue
+            for i in range(4):
+                l1 = changeLock(lock, i, True)
+                if (not l1 in visited) and (not l1 in deadends):
+                    visited[l1] = True
+                    q.append((l1, x+1))
+                l2 = changeLock(lock, i, False)
+                if not l2 in visited and (not l2 in deadends):
+                    visited[l2] = True
+                    q.append((l2, x+1))
+        return -1
