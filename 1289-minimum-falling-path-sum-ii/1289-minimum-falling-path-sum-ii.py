17d25f77e9086580842860d05ba7341830057c10
Austin Lee <haulrest@gmail.com>
2024-04-26 23:50:02 +0900

    Time: 216 ms (92.92%), Space: 20.1 MB (34.96%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        # Improved by reference
         n = len(grid)
 
         if n == 1:
@@ -8,8 +9,10 @@ class Solution:
         arr = [0]*n
 
         for i in range(n):
+            min1 = arr.index(min(arr))
+            min2 = arr.index(min(arr[:min1] + arr[min1+1:]))
             for j in range(n):
-                grid[i][j] = min(arr[:j] + arr[j+1:]) + grid[i][j]
+                grid[i][j] += (arr[min1] if min1 != j else arr[min2])
             arr = grid[i]
         
         return min(arr)
