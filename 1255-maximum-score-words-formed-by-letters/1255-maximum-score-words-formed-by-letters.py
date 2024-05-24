f84b251ee723908b0264e21b217d0e18921b8f89
Austin Lee <haulrest@gmail.com>
2024-05-24 09:43:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -0,0 +1,32 @@
+class Solution:
+    ans = 0
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        
+        l = len(words)
+        counts = [0 for _ in range(26)]
+        for lett in letters:
+            counts[ord(lett)-ord('a')] += 1
+
+        def dfs(x):
+            val = 0
+            if x >= l:
+                return val
+            else:
+                w = words[x]
+                wc = [w.count(chr(ord('a') + i)) for i in range(26)]
+                
+                if all(wc[i] <= counts[i] for i in range(26)):
+                    for i in range(26):
+                        counts[i] -= wc[i]
+                    val = sum(wc[i]*score[i] for i in range(26)) + dfs(x+1)
+                    for i in range(26):
+                        counts[i] += wc[i]
+                else:
+                    val = dfs(x+1)
+                print(val)
+                self.ans = max(self.ans, val)
+                return val
+        
+        for i in range(l):
+            dfs(i)
+        return self.ans
