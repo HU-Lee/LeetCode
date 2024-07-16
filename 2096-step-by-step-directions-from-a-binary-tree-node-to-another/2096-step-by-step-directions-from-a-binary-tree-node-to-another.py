ea13fac9e2ac40b91788eca5692fc5c78652b724
Austin Lee <haulrest@gmail.com>
2024-07-16 10:17:07 +0900

    Time: 625 ms (24.16%), Space: 51.3 MB (72.48%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,21 +6,19 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        dic = {}
-        arr = []
-        def dfs(node: TreeNode):
-            dic[node.val] = ''.join(arr)
+        def findPath(node: TreeNode, value: int):
             if node.left:
-                arr.append('L')
-                dfs(node.left)
-                arr.pop()
+                ans = findPath(node.left, value)
+                if ans is not None:
+                    return 'L' + ans
             if node.right:
-                arr.append('R')
-                dfs(node.right)
-                arr.pop()
-        dfs(root)
-        startPath = dic[startValue]
-        destPath = dic[destValue]
+                ans = findPath(node.right, value)
+                if ans is not None:
+                    return 'R' + ans
+            return '' if node.val == value else None
+
+        startPath = findPath(root, startValue)
+        destPath  = findPath(root, destValue)
         while len(startPath) > 0 and len(destPath) > 0 and startPath[0] == destPath[0]:
             startPath = startPath[1:]
             destPath = destPath[1:]
